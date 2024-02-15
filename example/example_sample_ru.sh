3ba6e1b1043066b84cd6a2a3bc54bcd309c8d35b
PGXLS <pgxls@pgsuite.org>
2024-02-15 19:16:41 +0300

    Delete example/example_sample_ru.sh

--- example/example_sample_ru.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# 1. Создание файла по SQL-запросу
-psql -Aqt -c "select encode(pgxls.get_file_by_query('select * from pg_class'),'hex')" | xxd -r -ps > pg_class.xlsx
-
-# 2. Сохранение файла из SQL-функции  
-psql -Aqt -c "select encode(excel_top_relations_by_size(),'hex')" | xxd -r -ps > top_relations_by_size.xlsx
-
